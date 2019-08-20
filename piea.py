5798e04d2a0ae936dc986440beebc6ca53e3905b
Dmitry Matrokhin <matrokhin.d@gmail.com>
2019-08-20 20:51:27 +0300

    Fix issues from linters

--- piea.py
+++ piea.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Plot sunburst chart for given tree."""
 
 import argparse
 import sys
@@ -7,55 +8,58 @@ import plotly.offline as py
 
 
 def add_slash(path):
-    """Adds slash to the path if it's not there"""
+    """Add slash to the path if it's not there."""
     if path.endswith('/'):
         return path
     return path + '/'
 
 
 def split_path(path, all_paths):
-    """Splits path into parent and current folder"""
+    """Split path into parent and current folder."""
     idx = path.rfind('/', 0, -1)
     if idx == -1:
         return '', path
     parent, label = (path[0:idx + 1], path[idx+1:-1])
-    if not parent in all_paths:
+    if parent not in all_paths:
         return '', path
     return parent, label
 
 
 def plot(open_browser, filename):
-    """Plots sunburst chart from data given in stdin"""
+    """Plot sunburst chart from data given in stdin."""
     sizes, paths = zip(*[line.rstrip().split() for line in sys.stdin])
     sizes, paths = list(map(int, sizes)), list(map(add_slash, paths))
     paths_set = set(paths)
     parents, labels = zip(*map(lambda p: split_path(p, paths_set), paths))
 
     trace = go.Sunburst(
-            ids = paths,
-            labels = labels,
-            parents = parents,
-            values = sizes,
-            branchvalues = 'total')
+        ids=paths,
+        labels=labels,
+        parents=parents,
+        values=sizes,
+        branchvalues='total')
     fig = go.Figure([trace])
     py.plot(fig,
-            auto_open = open_browser,
-            filename = filename)
+            auto_open=open_browser,
+            filename=filename)
 
 
 def main():
-    """Main script function"""
+    """Main script function."""
     parser = argparse.ArgumentParser(
-            description = 'Plot sunburst chart for given tree')
+        description='Plot sunburst chart for given tree')
 
-    parser.add_argument('--hide',
-            action = 'store_true',
-            help = "don't open browser (default: open browser)")
-    parser.add_argument('--file',
-            default = 'temp_pie.html',
-            help = "name of output html file")
+    parser.add_argument(
+        '--hide',
+        action='store_true',
+        help="don't open browser (default: open browser)")
+    parser.add_argument(
+        '--file',
+        default='temp_pie.html',
+        help="name of output html file")
 
     args = parser.parse_args()
     plot(not args.hide, args.file)
+
 
 main()
